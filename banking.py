e4bc5d131c2b49b276d1a9bb46fadc70b8808ffd
majiddarvishnejad93 <majiddarvishnejad93@gmail.com>
2020-11-22 15:46:11 +0330

    Finish project

--- banking.py
+++ banking.py
@@ -2,39 +2,70 @@
 from random import randint
 
 
-def handle_func(entry_number, entry_card_detail=None):
+def handle_func():
+    card = CreateCard()
+    entry_number = int(input())
+    global carder
     if entry_number == 1:
         carder = card.create_card()
-        print("Press 2 if you want to login")
-        return carder
+        # print("Press 2 if you want to login")
+        handle_func()
     elif entry_number == 2:
-        print(entry_card_detail)
-        print("Enter card number")
+        card_number = carder.get('card_number')
+        pin_number = carder.get('pin_number')
+        print("Enter your card number:")
+        card_number_input = int(input())
+        print("Enter your PIN:")
+        pin_number_input = int(input())
+        if pin_number != pin_number_input or card_number != card_number_input:
+            print("Wrong card number or PIN!")
+            again = int(input())
+            handle_func_entered(again)
+            return
+        else:
+            print("You have successfully logged in!")
+            again = int(input())
+            handle_func_entered(again)
+            # print('Press 1 for balance')
     else:
-        print('Exit')
+        print('Bye!')
+
+
+def handle_func_entered(entry_arg):
+    print("Press 1 or 2")
+    if entry_arg == 1:
+        print(entry_arg * 235)
+        balance = int(input())
+        handle_func_entered(balance)
+    elif entry_arg == 2:
+        print("You have successfully logged out!")
+        return
+    else:
+        print('Bye!')
 
 
 class CreateCard:
     def __init__(self):
         self.mii_number = randint(0, 9)
         self.bin_number = randint(10000, 99999)
-        self.account_identifier = randint(100000000, 999999999)
+        self.account_identifier = randint(1000000000, 9999999999)
         self.check_sum = randint(0, 9)
         self.card_number = self.mii_number + self.bin_number + self.account_identifier + self.check_sum
         self.card_pin = randint(0000, 9999)
 
     def create_card(self):
+        card_number_generated = f"400000{self.card_number}"
+        print("Your card has been created")
+        print("Your card number:")
+        print(card_number_generated)
+        print("Your card PIN:")
+        print(self.card_pin)
         created_card = {
-            "card_number": self.card_number,
+            "card_number": int(card_number_generated),
             "pin_number": self.card_pin
         }
         return created_card
 
 
-card = CreateCard()
-
-print("Press 1 for creating card")
-task = int(input())
-card_detail = handle_func(task)
-task2 = int(input())
-handle_func(task2, card_detail)
+# print("Press 1 for creating card")
+handle_func()
